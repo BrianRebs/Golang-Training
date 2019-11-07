f014eb6c4fa166555b8a10164a40697ef600eb05
Brian <brian@Servidor.local>
2019-11-07 17:16:00 -0600

    Chapter 2

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
-import "fmt"
+// Chaprter 2 
+
+package main
+
+// Run this file, then go to localhost:8080.
+// Type in localhost:8080/YourName
+// You will see the server print out "Hello YourName" on the page
+
+import (
+	"net/http"
+	"strings"
+)
+
+func sayHello(w http.ResponseWriter, r *http.Request) {
+	m := r.URL.Path
+	m = strings.TrimPrefix(m, "/")
+	m = "Hello " + m
+
+	// convert to a slice of bytes for the Writer
+	w.Write([]byte(m))
+}
 
 func main() {
-	fmt.Println("Hello World")
+	http.HandleFunc("/", sayHello)
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
 }
